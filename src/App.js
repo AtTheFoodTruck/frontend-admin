9d7b66c2b68ff48b929b81413a7e75b0d822609d
Ojea <rnjsdhwo1325@gmail.com>
2022-04-27 06:46:44 +0900

    Api수정 필요함

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Container } from 'react-bootstrap';
-import 'bootswatch/dist/sketchy/bootstrap.css';
+import { Container } from "react-bootstrap";
+import "bootswatch/dist/sketchy/bootstrap.css";
 
-import './App.css';
-import MenuRegister from './components/MenuRegister';
-import OrderHistory from './components/OrderHistory';
+import "./App.css";
+import OrderHistory from "./components/OrderHistory";
+import MenuRegister from "./components/DashBoard/MenuRegister/MenuRegister";
+import Ex from "./components/DashBoard/MenuRegister/Ex";
 
 function App() {
   return (
     <Container>
       <MenuRegister />
       {/* <OrderHistory /> */}
+      <Ex />
     </Container>
   );
 }
